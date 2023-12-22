fc0e97222fe00b410cc1dd5e4b90c4c0989299c2
David Pontes <pondavid103@gmail.com>
2023-12-22 01:10:41 -0300

    Resolvendo Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia1= new Nokia(numero:"345758494", modelo:"Modelo1", imei: "438393939", memoria: 120);
+nokia1.Ligar();
+nokia1.InstalarAplicativo("whatsApp");
+
+Smartphone iphone1= new Iphone(numero:"987351560", modelo:"Modelo2", imei: "4938292949", memoria: 240);
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Facebook");
